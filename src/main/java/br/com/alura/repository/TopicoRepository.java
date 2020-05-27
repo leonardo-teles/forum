329129091c096cc553fd57e8b0fd3bc188cf2249
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-26 23:18:55 -0300

    Consulta com Filtros

--- src/main/java/br/com/alura/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/repository/TopicoRepository.java
@@ -1,5 +1,7 @@
 package br.com.alura.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import br.com.alura.modelo.Topico;
 
 @Repository
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	List<Topico> findByCursoNome(String nomeCurso);
 
 }
