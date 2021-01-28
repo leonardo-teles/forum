99d3d4787c7c9ffb32299132b7457b34b09973d8
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-28 01:27:45 -0300

    Enviando Token JWT no Swagger

--- src/main/java/br/com/alura/config/SwaggerConfig.java
+++ src/main/java/br/com/alura/config/SwaggerConfig.java
@@ -1,11 +1,15 @@
 package br.com.alura.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.alura.modelo.Usuario;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -19,6 +23,14 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("br.com.alura"))
                 .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(Usuario.class);
+                .ignoredParameterTypes(Usuario.class)
+                .globalOperationParameters(Arrays.asList(
+                		new ParameterBuilder()
+                			.name("Authorization")
+                			.description("Header para token JWT")
+                			.modelRef(new ModelRef("string"))
+                			.parameterType("header")
+                			.required(false)
+                			.build()));
 	}
 }
